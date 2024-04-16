10b4909d5a96c74ae6c6d07ec9ad8c693869ce6d
niedlich3 <126873114+niedlich3@users.noreply.github.com>
2024-04-16 11:07:06 +0200

    Update wyszukiwanie.php

--- wyszukiwanie.php
+++ wyszukiwanie.php
@@ -4,7 +4,9 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Wyszukaj wydarzenie</title>
-	<link rel="stylesheet" href="kurwastyl.css">
+	<link rel="stylesheet" href="styl_pyknijmy.css">
+
+
 </head>
 <body>
 	
