885d592694a7cc7d4ffe61854d5a27cdc9fcb262
niedlich3 <126873114+niedlich3@users.noreply.github.com>
2024-04-23 13:25:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -82,12 +82,7 @@
 	
 	
 	
-	 $host = 'localhost'; 
-	 $username = 'root';
-	 $password = ''; 
-	 $database = 'pyknijmy'; 
-	 
-	 $conn = mysqli_connect($host, $username, $password, $database);
+	
 	 
 	 if (!$conn) {
 		 die("Błąd połączenia: " . mysqli_connect_error());
