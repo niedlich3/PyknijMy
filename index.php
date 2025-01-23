b8bf85a552b240af2391cc1863108a9c9ec23b6f
niedlich3 <126873114+niedlich3@users.noreply.github.com>
2025-01-23 15:39:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="theme-color" content="#000000"/>
+	<title>PyknijMy</title>
+	<link rel="icon" type="image/png" href="grafika/ikonka_pyknijmy.png">
+	<link rel="stylesheet" href="css/styl_pyknijmy2.css">
+	<link rel="manifest" href="manifest.json">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            
+            <a class="navbar-brand" href = "index.php">  <img src="grafika/logopyknijmy.png" width="75" height="75" class="d-inline-block align-text-top">
+               
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a href = "index.php" class="nav-link active" aria-current="page" >Strona główna</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#uslugi">Usługi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#aboutus">O nas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kontakt</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-primary" href="#">Dołącz teraz</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+<main>
+	 <div class="uslugi">
+        <div class="image-container">
+           <a href ="sport-wydarzenia.php"> <img src="grafika/sport.png" alt="1" class="img-fluid"></a>
+        </div>
+        <div class="image-container">
+           <a href="nauka-wydarzenia.php"> <img src="grafika/nauka.png" alt="2" class="img-fluid"></a>
+        </div>
+        <div class="image-container">
+            <a href="rozrywka-wydarzenia.php"> <img src="grafika/rozrywka.png" alt="3" class="img-fluid"></a>
+        </div>
+</main>
+<footer>
+		<div class = "footerContainer">
+		 <div class = "footerNav">
+		  <ul>
+		    <li><a href = "index.php">Home</a></li>	
+			<li><a href= "https://linktr.ee/beatbuddy" id ="aboutus">About Us</a></li>
+		
+		  </ul>
+		 </div>
+		 
+		</div>
+		<div class = "footerBottom">
+		  <p>Copyright &copy;2024; Designed by <span class = "designer">BIG$WAGGBOY$BAND</span></p>
+		 </div>
+		</footer>
+		<script src="script.js"></script>
+</body>
+</html>
