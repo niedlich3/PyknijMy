f0340754fdbb6818447d946dc0290323df575aeb
okolacki <129507504+okolacki@users.noreply.github.com>
2025-05-21 22:44:47 +0200

    Add files via upload

--- pyknijmy.sql
+++ pyknijmy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 20 Maj 2025, 19:51
--- Wersja serwera: 10.4.27-MariaDB
--- Wersja PHP: 8.2.0
+-- Generation Time: Maj 21, 2025 at 10:43 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `pyknijmy`
+-- Database: `pyknijmy`
 --
 
 -- --------------------------------------------------------
@@ -37,21 +37,18 @@ CREATE TABLE `users` (
   `about_me` text NOT NULL,
   `profile_image` longblob NOT NULL,
   `birthdate` date DEFAULT NULL,
-  `gender` enum('male','female','other') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `gender` enum('male','female','other') NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0
+) ;
 
 --
--- Zrzut danych tabeli `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user_id`, `user_name`, `email`, `password`, `date`, `about_me`, `profile_image`, `birthdate`, `gender`) VALUES
-(1, 3199144809510910116, 'szymon', 'nied@pp', '$2y$10$yrd6CvqacatpSdcbknQoo.3.zlJ21iUcptnGZ1t1L1PhDPrH2Igqe', '2025-04-24 10:41:00', 'Podoba mi sie Remek', '', NULL, 'male'),
-(2, 328727, 'wae', 'ea@2a', '$2y$10$.uo8LC6KGpWmPniMHSRW..4WwOi7iN16/XDbcW0goAdwT7jKKRwg.', '2025-04-03 12:43:55', '', '', NULL, 'male'),
-(3, 589910, 'admin', 'remuwson@13', '$2y$10$l/VpRuJP1viu92dkwMTn1.QH3UPEcQapKz1/q1TQdYzHWuTc7H3Dq', '2025-04-08 15:51:42', '', '', NULL, 'male'),
-(4, 685643601, 'remek', 'remek@12', '$2y$10$5zTZi7HPz5bSVLXus.5SR.FSZivt8mYnJ.pCbRlqbJvPzAyjmLFJa', '2025-04-24 13:02:26', 'jestem glupi tzfl!', '', NULL, 'male'),
-(5, 8572421547762, 'asd', 'asd@asd', '$2y$10$8Y6/46KYahGJLZG/5ZsvBe7dzxe3Pzp2.v/Us.4S60Y/6fPTVfDY6', '2025-04-24 12:56:11', 'jestem glupi', '', NULL, 'male'),
-(10, 41405702141407, 'vfeve', 'qwerty@pp', '$2y$10$r8Ea7Kp7hLiJbpEB0dszX.gX2p6V/9KQB/J/c.WdHLorKYiWs3hxy', '2025-04-24 14:25:31', 'Cześćwe', '', '2025-03-31', 'female'),
-(11, 39356086, 'niedlich', 'nied@wp.pl', '$2y$10$uDkJKjehgomSoqZHE.344uhf8AUT3mvbDqH6wous0fihNrmqJoEgW', '2025-05-20 17:47:47', '', '', '2025-04-30', 'other');
+INSERT INTO `users` (`id`, `user_id`, `user_name`, `email`, `password`, `date`, `about_me`, `profile_image`, `birthdate`, `gender`, `is_admin`) VALUES
+(11, 39356086, 'niedlich', 'nied@wp.pl', '$2y$10$uDkJKjehgomSoqZHE.344uhf8AUT3mvbDqH6wous0fihNrmqJoEgW', '2025-05-20 17:47:47', '', '', '2025-04-30', 'other', 0),
+(13, 79764077063404, 'borys', 'b@b.pl', '$2y$10$nGkjWKvK.NKpHnnRW1nIZOHDI/mh05EhRE8h4tpEwWnROH7sDLM86', '2025-05-21 14:18:48', '', '', NULL, 'male', 0),
+(14, 863010463534, 'admin', 'admin@admin.pl', '$2y$10$R.h9HQA8rsrfdeMmkr/nCetBpErKwSLnB7R7Kj1YyY3Rk.dDyqg6i', '2025-05-21 14:39:56', '', '', NULL, 'male', 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -64,14 +61,14 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
